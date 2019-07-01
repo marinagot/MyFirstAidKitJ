a609f5c33ea856719f4def269223f23045422f8d
itum95 <43616806+itum95@users.noreply.github.com>
2019-07-01 12:11:05 +0200

    Cambios realizados

--- app/src/main/java/com/example/myfirstaidkit/treatment_edit.java
+++ app/src/main/java/com/example/myfirstaidkit/treatment_edit.java
@@ -1,8 +1,6 @@
 package com.example.myfirstaidkit;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,13 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 import com.example.myfirstaidkit.data.DataBaseOperations;
 import com.example.myfirstaidkit.data.Treatment;
-
-import androidx.navigation.Navigation;
 
 
 /**
