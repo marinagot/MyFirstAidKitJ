aa9abe3bff88fd9b42b5787881bc08682725ebaa
John-Development <38006800+John-Development@users.noreply.github.com>
2020-02-26 16:31:15 +0100

    Un poco mas de lista expandible
    Funcionalidad restaurada de editar y eliminar tratamiento (aunque lo ultimo no esta probado)

--- app/src/main/java/com/example/myfirstaidkit/TreatmentsListAdapter.java
+++ app/src/main/java/com/example/myfirstaidkit/TreatmentsListAdapter.java
@@ -100,81 +100,67 @@ class TreatmentsListAdapter<T> extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(final int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        dbo = DataBaseOperations.get_Instance(context);
         if (convertView == null) {
             convertView = inflater.inflate(style, null);
         }
 
-        Treatment treatment = (Treatment) getGroup(groupPosition);
+        final Treatment t = (Treatment) getGroup(groupPosition);
 
-        TextView listTitleTextView = convertView.findViewById(R.id.treatment_item_header);
-        listTitleTextView.setTypeface(null, Typeface.BOLD);
-        listTitleTextView.setText(treatment.getName());
+        TextView header = convertView.findViewById(R.id.treatment_item_header);
+        TextView text = convertView.findViewById(R.id.treatment_item_text);
+        header.setText(t.getName());
+        text.setText(t.getIdUser());
+        ImageButton editImageView = convertView.findViewById(R.id.treatment_item_edit);
+        editImageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Gson gson = new Gson();
+                String treatment = null;
+                String medicines = null;
+                List<MedTretRel> list_rel = dbo.getRelations_treatmentId(t.getId());
+                List<Medicine> list_med = new ArrayList<>();
+
+                for (MedTretRel mtr : list_rel ) {
+                    list_med.add(dbo.getMedicine_medicineId(mtr.getIdMedicine()));
+                }
+
+                String relations = null;
+                try {
+                    treatment = new JSONObject(gson.toJson(t)).toString();
+                    medicines = new JSONArray(gson.toJson(list_med)).toString();
+                    relations = new JSONArray(gson.toJson(list_rel)).toString();
+                } catch (Exception e) { }
+
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("isTreatmentEdit", true);
+                bundle.putString("treatment", treatment);
+                bundle.putString("medicines", medicines);
+                bundle.putString("relations", relations);
+
+                Navigation.findNavController(v).navigate(R.id.action_treatments_to_treatment_edit, bundle);
+            }
+        });
+
+        ImageButton deleteImageView = convertView.findViewById(R.id.treatment_item_delete);
+        deleteImageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new ApiCallThread<String>(new AsyncResponse<String>(){
+                    @Override
+                    public String apiCall(Object... params) {
+                        return dbo.deleteTreatment((Treatment) params[1]);
+                    }
+
+                    @Override
+                    public void processFinish(View v, String result) {
+                        expandableListTitle.remove(t);
+                        expandableListDetail.remove(t);
+                        notifyDataSetChanged();
+                    }
+                }).execute(v, t);
+            }
+        });
+
         return convertView;
-
-//        View vi = convertView;
-//        us = DataBaseOperations.get_Instance(context);
-//        if (vi == null) {
-//            vi = inflater.inflate(style, null);
-//        }
-//
-//        TextView header = vi.findViewById(R.id.treatment_item_header);
-//        TextView text = vi.findViewById(R.id.treatment_item_text);
-//        header.setText(((Treatment) data.get(groupPosition)).getName());
-//        text.setText(((Treatment) data.get(groupPosition)).getIdUser());
-//
-//        ImageButton editImageView = vi.findViewById(R.id.treatment_item_edit);
-//        editImageView.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//
-//                Gson gson = new Gson();
-//
-//                String treatment = null;
-//                Treatment t = (Treatment) data.get(groupPosition);
-//
-//                String medicines = null;
-//                List<MedTretRel> list_rel = us.getRelations_treatmentId(t.getId());
-//                List<Medicine> list_med = new ArrayList<>();
-//                for (MedTretRel mtr : list_rel ) {
-//                    list_med.add(us.getMedicine_medicineId(mtr.getIdMedicine()));
-//                }
-//
-//                String relations = null;
-//
-//                try {
-//                    treatment = new JSONObject(gson.toJson(data.get(groupPosition))).toString();
-//                    medicines = new JSONArray(gson.toJson(list_med)).toString();
-//                    relations = new JSONArray(gson.toJson(list_rel)).toString();
-//                } catch (Exception e) { }
-//
-//                Bundle bundle = new Bundle();
-//                bundle.putBoolean("isTreatmentEdit", true);
-//                bundle.putString("treatment", treatment);
-//                bundle.putString("medicines", medicines);
-//                bundle.putString("relations", relations);
-//
-//                Navigation.findNavController(v).navigate(R.id.action_treatments_to_treatment_edit, bundle);
-//            }
-//        });
-//
-//        ImageButton deleteImageView = vi.findViewById(R.id.treatment_item_delete);
-//        deleteImageView.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                new ApiCallThread<String>(new AsyncResponse<String>(){
-//                    @Override
-//                    public String apiCall(Object... params) {
-//                        return us.deleteTreatment((Treatment) params[1]);
-//                    }
-//
-//                    @Override
-//                    public void processFinish(View v, String result){
-//                        remove(data.get(groupPosition));
-//                        notifyDataSetChanged();
-//                    }
-//                }).execute(v, data.get(groupPosition));
-//            }
-//        });
-//
-//        return vi;
     }
 
     @Override
